136e3e970e068534a19d944030c3132087c13bfd
leeyunjai82 <leeyunjai@circul.us>
2021-12-22 19:39:30 +0900

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@ from openpibo import __version__ as VERSION
 # -- Project information -----------------------------------------------------
 
 project = 'openpibo'
-copyright = f'2021, Circulus Education, openpibo-python-{VERSION} - THE MAKER'
+copyright = f'2021, Circulus Education, THE MAKER <openpibo-python-{VERSION}>'
 author = 'Circulus'
 
 # The full version, including alpha/beta/rc tags
