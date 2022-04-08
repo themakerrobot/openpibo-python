2d37b61ded8082094d610c42ac5736b5f7de687a
leeyunjai82 <leeyunjai@circul.us>
2022-04-08 11:07:11 +0900

    update

--- openpibo/speech.py
+++ openpibo/speech.py
@@ -212,7 +212,7 @@ Functions:
 
       pibo_dialog.load(filepath='/home/pi/dialog.csv')
 
-    :param str string: 대화 파일 (csv)
+    :param str string: 분석할 문장 (한글)
 
       아래 형태의 ``csv 파일`` 만 사용 가능합니다.
       파일 내용::
