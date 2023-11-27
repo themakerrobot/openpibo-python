45c625f95738326b3556df3e318423ddeb746d49
leeyunjai82 <leeyunjai@circul.us>
2023-11-27 10:00:34 +0900

    update

--- openpibo/device.py
+++ openpibo/device.py
@@ -276,7 +276,7 @@ Functions:
   Device가 사용하는 ``code_list`` 정보입니다.
   """
 
-  def __init__(self, api_mode=True, port=80):
+  def __init__(self, api_mode=False, port=80):
     """
     Device 클래스를 초기화합니다.
     """
