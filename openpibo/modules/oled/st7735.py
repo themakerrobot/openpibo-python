6a76e501cc1f619e4b8afd07e02badd980ac5d08
leeyunjai82 <leeyunjai@circul.us>
2023-12-27 16:21:13 +0900

    update

---
+++ openpibo/modules/oled/st7735.py
@@ -0,0 +1,312 @@
+# SPDX-FileCopyrightText: 2017 Radomir Dopieralski for Adafruit Industries
+# SPDX-FileCopyrightText: 2023 Matt Land
+#
+# SPDX-License-Identifier: MIT
+
+"""
+`adafruit_rgb_display.st7735`
+====================================================
+
+A simple driver for the ST7735-based displays.
+
+* Author(s): Radomir Dopieralski, Michael McWethy, Matt Land
+"""
+import struct
+
+from .rgb import DisplaySPI
+
+try:
+    from typing import Optional, Tuple, ByteString, Union
+    from . import digitalio as digitalio
+    from . import busio as busio
+except ImportError:
+    pass
+
+__version__ = "0.0.0+auto.0"
+__repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_RGB_Display.git"
+
+_NOP = 0x00
+_SWRESET = 0x01
+_RDDID = 0x04
+_RDDST = 0x09
+
+_SLPIN = 0x10
+_SLPOUT = 0x11
+_PTLON = 0x12
+_NORON = 0x13
+
+_INVOFF = 0x20
+_INVON = 0x21
+_DISPOFF = 0x28
+_DISPON = 0x29
+_CASET = 0x2A
+_RASET = 0x2B
+_RAMWR = 0x2C
+_RAMRD = 0x2E
+
+_PTLAR = 0x30
+_COLMOD = 0x3A
+_MADCTL = 0x36
+
+_FRMCTR1 = 0xB1
+_FRMCTR2 = 0xB2
+_FRMCTR3 = 0xB3
+_INVCTR = 0xB4
+_DISSET5 = 0xB6
+
+_PWCTR1 = 0xC0
+_PWCTR2 = 0xC1
+_PWCTR3 = 0xC2
+_PWCTR4 = 0xC3
+_PWCTR5 = 0xC4
+_VMCTR1 = 0xC5
+
+_RDID1 = 0xDA
+_RDID2 = 0xDB
+_RDID3 = 0xDC
+_RDID4 = 0xDD
+
+_PWCTR6 = 0xFC
+
+_GMCTRP1 = 0xE0
+_GMCTRN1 = 0xE1
+
+
+class ST7735(DisplaySPI):
+    """
+    A simple driver for the ST7735-based displays.
+
+    >>> import busio
+    >>> import digitalio
+    >>> import board
+    >>> from adafruit_rgb_display import color565
+    >>> import adafruit_rgb_display.st7735 as st7735
+    >>> spi = busio.SPI(clock=board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+    >>> display = st7735.ST7735(spi, cs=digitalio.DigitalInOut(board.GPIO0),
+    ...    dc=digitalio.DigitalInOut(board.GPIO15), rst=digitalio.DigitalInOut(board.GPIO16))
+    >>> display.fill(0x7521)
+    >>> display.pixel(64, 64, 0)
+    """
+
+    _COLUMN_SET = _CASET
+    _PAGE_SET = _RASET
+    _RAM_WRITE = _RAMWR
+    _RAM_READ = _RAMRD
+    _INIT = (
+        (_SWRESET, None),
+        (_SLPOUT, None),
+        (_COLMOD, b"\x05"),  # 16bit color
+        # fastest refresh, 6 lines front porch, 3 line back porch
+        (_FRMCTR1, b"\x00\x06\x03"),
+        (_MADCTL, b"\x08"),  # bottom to top refresh
+        # 1 clk cycle nonoverlap, 2 cycle gate rise, 3 sycle osc equalie,
+        # fix on VTL
+        (_DISSET5, b"\x15\x02"),
+        (_INVCTR, b"0x00"),  # line inversion
+        (_PWCTR1, b"\x02\x70"),  # GVDD = 4.7V, 1.0uA
+        (_PWCTR2, b"\x05"),  # VGH=14.7V, VGL=-7.35V
+        (_PWCTR3, b"\x01\x02"),  # Opamp current small, Boost frequency
+        (_VMCTR1, b"\x3c\x38"),  # VCOMH = 4V, VOML = -1.1V
+        (_PWCTR6, b"\x11\x15"),
+        (
+            _GMCTRP1,
+            b"\x09\x16\x09\x20\x21\x1b\x13\x19" b"\x17\x15\x1e\x2b\x04\x05\x02\x0e",
+        ),  # Gamma
+        (
+            _GMCTRN1,
+            b"\x08\x14\x08\x1e\x22\x1d\x18\x1e" b"\x18\x1a\x24\x2b\x06\x06\x02\x0f",
+        ),
+        (_CASET, b"\x00\x02\x00\x81"),  # XSTART = 2, XEND = 129
+        (_RASET, b"\x00\x02\x00\x81"),  # XSTART = 2, XEND = 129
+        (_NORON, None),
+        (_DISPON, None),
+    )  # type: Tuple[Tuple[int, Union[ByteString, None]], ...]
+    _ENCODE_PIXEL = ">H"
+    _ENCODE_POS = ">HH"
+
+    # pylint: disable-msg=useless-super-delegation, too-many-arguments
+    def __init__(
+        self,
+        spi: busio.SPI,
+        dc: digitalio.DigitalInOut,
+        cs: digitalio.DigitalInOut,
+        rst: Optional[digitalio.DigitalInOut] = None,
+        width: int = 128,
+        height: int = 128,
+        baudrate: int = 16000000,
+        polarity: int = 0,
+        phase: int = 0,
+        *,
+        x_offset: int = 0,
+        y_offset: int = 0,
+        rotation: int = 0,
+    ) -> None:
+        super().__init__(
+            spi,
+            dc,
+            cs,
+            rst,
+            width,
+            height,
+            baudrate=baudrate,
+            polarity=polarity,
+            phase=phase,
+            x_offset=x_offset,
+            y_offset=y_offset,
+            rotation=rotation,
+        )
+
+
+class ST7735R(ST7735):
+    """A simple driver for the ST7735R-based displays."""
+
+    _INIT = (
+        (_SWRESET, None),
+        (_SLPOUT, None),
+        (_MADCTL, b"\xc8"),
+        (_COLMOD, b"\x05"),  # 16bit color
+        (_INVCTR, b"\x07"),
+        (_FRMCTR1, b"\x01\x2c\x2d"),
+        (_FRMCTR2, b"\x01\x2c\x2d"),
+        (_FRMCTR3, b"\x01\x2c\x2d\x01\x2c\x2d"),
+        (_PWCTR1, b"\x02\x02\x84"),
+        (_PWCTR2, b"\xc5"),
+        (_PWCTR3, b"\x0a\x00"),
+        (_PWCTR4, b"\x8a\x2a"),
+        (_PWCTR5, b"\x8a\xee"),
+        (_VMCTR1, b"\x0e"),
+        (_INVOFF, None),
+        (
+            _GMCTRP1,
+            b"\x02\x1c\x07\x12\x37\x32\x29\x2d" b"\x29\x25\x2B\x39\x00\x01\x03\x10",
+        ),  # Gamma
+        (
+            _GMCTRN1,
+            b"\x03\x1d\x07\x06\x2E\x2C\x29\x2D" b"\x2E\x2E\x37\x3F\x00\x00\x02\x10",
+        ),
+    )
+
+    # pylint: disable-msg=useless-super-delegation, too-many-arguments
+    def __init__(
+        self,
+        spi: busio.SPI,
+        dc: digitalio.DigitalInOut,
+        cs: digitalio.DigitalInOut,
+        rst: Optional[digitalio.DigitalInOut] = None,
+        width: int = 128,
+        height: int = 160,
+        baudrate: int = 16000000,
+        polarity: int = 0,
+        phase: int = 0,
+        *,
+        x_offset: int = 0,
+        y_offset: int = 0,
+        rotation: int = 0,
+        bgr: bool = False,
+        invert: bool = False,
+    ) -> None:
+        self._bgr = bgr
+        self._invert = invert
+        super().__init__(
+            spi,
+            dc,
+            cs,
+            rst,
+            width,
+            height,
+            baudrate=baudrate,
+            polarity=polarity,
+            phase=phase,
+            x_offset=x_offset,
+            y_offset=y_offset,
+            rotation=rotation,
+        )
+
+    def init(self) -> None:
+        super().init()
+        cols = struct.pack(">HH", 0, self.width - 1)
+        rows = struct.pack(">HH", 0, self.height - 1)
+
+        for command, data in (
+            (_CASET, cols),
+            (_RASET, rows),
+            (_NORON, None),
+            (_DISPON, None),
+        ):
+            self.write(command, data)
+        if self._bgr:
+            self.write(_MADCTL, b"\xc0")
+        if self._invert:
+            self.write(_INVON, None)
+
+
+class ST7735S(ST7735):
+    """A simple driver for the ST7735S-based displays."""
+
+    _INIT = (
+        # Frame Rate
+        (_FRMCTR1, b"\x01\x2c\x2d"),
+        (_FRMCTR2, b"\x01\x2c\x2d"),
+        (_FRMCTR3, b"\x01\x2c\x2d\x01\x2c\x2d"),
+        # Column inversion
+        (_INVCTR, b"\x07"),
+        # Power Sequence
+        (_PWCTR1, b"\xa2\x02\x84"),
+        (_PWCTR2, b"\xc5"),
+        (_PWCTR3, b"\x0a\x00"),
+        (_PWCTR4, b"\x8a\x2a"),
+        (_PWCTR5, b"\x8a\xee"),
+        # VCOM
+        (_VMCTR1, b"\x0e"),
+        # Gamma
+        (
+            _GMCTRP1,
+            b"\x0f\x1a\x0f\x18\x2f\x28\x20\x22" b"\x1f\x1b\x23\x37\x00\x07\x02\x10",
+        ),
+        (
+            _GMCTRN1,
+            b"\x0f\x1b\x0f\x17\x33\x2c\x29\x2e" b"\x30\x30\x39\x3f\x00\x07\x03\x10",
+        ),
+        # 65k mode
+        (_COLMOD, b"\x05"),
+        # set scan direction: up to down, right to left
+        (_MADCTL, b"\x60"),
+        (_SLPOUT, None),
+        (_DISPON, None),
+    )
+
+    # pylint: disable-msg=useless-super-delegation, too-many-arguments
+    def __init__(
+        self,
+        spi: busio.SPI,
+        dc: digitalio.DigitalInOut,
+        cs: digitalio.DigitalInOut,
+        bl: digitalio.DigitalInOut,  # Backlight
+        rst: Optional[digitalio.DigitalInOut] = None,
+        width: int = 128,
+        height: int = 160,
+        baudrate: int = 16000000,
+        polarity: int = 0,
+        phase: int = 0,
+        *,
+        x_offset: int = 2,
+        y_offset: int = 1,
+        rotation: int = 0,
+    ) -> None:
+        self._bl = bl
+        # Turn on backlight
+        self._bl.switch_to_output(value=1)
+        super().__init__(
+            spi,
+            dc,
+            cs,
+            rst,
+            width,
+            height,
+            baudrate=baudrate,
+            polarity=polarity,
+            phase=phase,
+            x_offset=x_offset,
+            y_offset=y_offset,
+            rotation=rotation,
+        )
--- openpibo/oled.py
+++ openpibo/oled.py
@@ -5,7 +5,7 @@ Class:
 :obj:`~openpibo.oled.Oled`
 """
 
-from .modules.oled import ssd1306, board, busio, digitalio
+from .modules.oled import st7735, ssd1306, board, busio, digitalio
 from PIL import Image, ImageDraw, ImageFont, ImageOps
 import cv2,os
 import numpy as np
@@ -278,4 +278,270 @@ Functions:
     """
 
     self.image = ImageOps.invert(self.image.convert("L")).convert("1")
+
+class OledRGB:
+  """
+Functions:
+:meth:`~openpibo.oled.OledRGB.show`
+:meth:`~openpibo.oled.OledRGB.clear`
+:meth:`~openpibo.oled.OledRGB.set_font`
+:meth:`~openpibo.oled.OledRGB.draw_text`
+:meth:`~openpibo.oled.OledRGB.draw_image`
+:meth:`~openpibo.oled.OledRGB.draw_data`
+:meth:`~openpibo.oled.OledRGB.draw_rectangle`
+:meth:`~openpibo.oled.OledRGB.draw_ellipse`
+:meth:`~openpibo.oled.OledRGB.draw_line`
+:meth:`~openpibo.oled.OledRGB.invert`
+
+  파이보의 OLED를 통해 다양한 그림을 표현합니다.
+
+  * 사진 보기
+  * 문자 그리기
+  * 도형 그리기
+
+  그림을 그리면 인스턴스 변수 ``image`` 에 저장됩니다. 이를 ``show`` 메소드를 사용하여 oled 화면에 출력할 수 있습니다.
+
+  본 class에서 문자 또는 그림을 그리는 행위는 인스턴스 변수 ``image`` 의 데이터를 변경시키는 것으로 정의합니다.
+
+  example::
+
+    from openpibo.oled import OledRGB
+
+    pibo_oled = OledRGB()
+    # 아래의 모든 예제 이전에 위 코드를 먼저 사용합니다.
+  """
+
+  def __init__(self, w=128, h=64):
+    """
+    Oled 클래스를 초기화
+    """
+
+    self.width = w
+    self.height = h
+    self.font_path = openpibo_models.filepath("KDL.ttf") # KoPub Dotum Light
+    self.font_size = 10
+
+    cs_pin = digitalio.DigitalInOut(board.D8)
+    dc_pin = digitalio.DigitalInOut(board.D23)
+    reset_pin = digitalio.DigitalInOut(board.D24)
+
+    spi = busio.SPI(11,10,9)
+    self.oled = st7735.ST7735S(spi, rotation=270, width=self.height, height=self.width, cs=cs_pin, bl=cs_pin, dc=dc_pin, rst=reset_pin,  x_offset=1, y_offset=1)
+    self.font = ImageFont.truetype(self.font_path, self.font_size)
+    self.image = Image.new("RGB", (self.width, self.height), (0,0,0))
+    self.oled.fill(0)
+
+  def show(self):
+    """
+    인스턴스 변수 ``image`` 를 oled 화면에 표시합니다.
+
+    **이 메소드를 사용하지 않으면 그림을 그려도 oled 화면에 아무것도 출력되지 않습니다.**
+
+    example::
+
+      # 그림을 그린 후
+
+      pibo_oled.show()
+    """
+
+    self.oled.image(self.image)
+
+  def clear(self, image=True, fill=True, show=True):
+    """
+    인스턴스 변수 ``image`` 를 초기화 하고, oled 화면을 지웁니다.
+
+    example::
+
+      pibo_oled.clear()
+
+    :param bool image: image 초기화 여부
+
+    :param bool fill: oled 초기화 여부
+
+    :param bool show: 화면 표시 여부
+    """
+
+    if image == True:
+      self.image = Image.new("RGB", (self.width, self.height), (0,0,0))
+    if fill == True:
+      self.oled.fill(0)
+    if show == True:
+      self.oled.image(image)
+
+  def set_font(self, filename=None, size=None):
+    """
+    ``draw_text`` 메소드에 사용할 폰트를 설정합니다.
+
+    example::
+
+      # 불러올 폰트의 경로가 /home/pi/mydata/font.ttf 라면,
+
+      pibo_oled.set_font('/home/pi/mydata/font.ttf', 10)
+
+    :param str filename: 폰트 파일 경로
+
+      폰트 확장자는 **ttf** 와 **otf** 모두 지원합니다.
+
+    :param int size: 폰트 사이즈
+
+      단위는 픽셀 입니다. (default 10)
+    """
+
+    if filename == None:
+      filename = self.font_path
+
+    if size == None:
+      size = self.font_size
+
+    if not os.path.isfile(filename):
+      raise Exception(f'"{filename}" does not exist')
+
+    self.font = ImageFont.truetype(filename, size)
+
+  def draw_text(self, points, text:str):
+    """
+    문자를 그립니다.(한글, 영어 지원)
+
+    example::
+
+      pibo_oled.draw_text((10, 10), '안녕하세요!')
+
+    :param tuple(int, int) points: 문자열 좌측상단 좌표 (x, y)
+
+    :param str text: 문자열 내용
+    """
+
+    if type(points) is not tuple:
+      raise Exception(f'"{points}" must be tuple type')
+
+    if len(points) != 2:
+      raise Exception(f'len({points}) must be 2')
+
+    ImageDraw.Draw(self.image).text(points, text, font=self.font, fill=255)
+
+  def draw_image(self, filename):
+    """
+    그림을 그립니다.
+
+    **128x64** 크기의 **png** 확장자만 허용됩니다.
+
+    example::
+
+      pibo_oled.draw_image('/home/pi/openpibo-files/image/clear.png')
+
+    :param str filename: 그림파일 경로
+    """
+
+    if not os.path.isfile(filename):
+      raise Exception(f'"{filename}" does not exist') 
+
+    self.image = Image.open(filename).resize((self.width, self.height))
+
+  def draw_data(self, img):
+    """
+    numpy 이미지 데이터를 입력받아 이미지로 변환합니다.
+
+    카메라 출력값이 numpy 형식이므로, 이를 oled화면에 띄우기 위해 사용됩니다.
+
+    example::
+
+      from openpibo.vision import Camera
+
+      pibo_camera = Camera()
+      img = pibo_camera.read()
+
+      pibo_oled.draw_data(img)
+      pibo_oled.show()
+
+    :param numpy.ndarray img: 이미지 객체
+    """
+
+    if type(img) is not np.ndarray:
+      raise Exception('"img" must be image data from opencv.')
+
+    self.image = Image.fromarray(img).resize((self.width, self.height))
+
+  def draw_rectangle(self, points, fill=None):
+    """
+    직사각형을 그립니다.
+
+    example::
+
+      pibo_oled.draw_rectangle((10, 10, 80, 40), True)
+
+    :param tuple points: 사각형의 좌측상단 좌표, 사각형의 우측하단 좌표 (x1, y1, x2, y2)
+
+    :param bool fill:
+
+      * ``True`` : 사각형 내부를 채웁니다.
+      * ``False`` : 사각형 내부를 채우지 않습니다.
+    """
+
+    if type(points) is not tuple:
+      raise Exception(f'"{points}" must be tuple type')
+
+    if len(points) != 4:
+      raise Exception(f'len({points}) must be 4')
+
+    if not fill in [None, True, False]:
+      raise Exception(f'"{fill}" must be (None|True|False)')
+
+    ImageDraw.Draw(self.image).rectangle(points, outline=1, fill=fill)
+
+  def draw_ellipse(self, points, fill=None):
+    """
+    타원을 그립니다.
+
+    example::
+
+      pibo_oled.draw_ellipse((10, 10, 80, 40), False)
+
+    :param tuple points: 타원에 외접하는 직사각형의 좌측상단 좌표, 우측하단 좌표 (x1, y1, x2, y2)
+
+    :param bool fill:
+
+      * ``True`` : 타원 내부를 채웁니다.
+      * ``False`` : 타원 내부를 채우지 않습니다.
+    """
+
+    if type(points) is not tuple:
+      raise Exception(f'"{points}" must be tuple type')
+
+    if len(points) != 4:
+      raise Exception(f'len({points}) must be 4')
+
+    if not fill in [None, True, False]:
+      raise Exception(f'"{fill}" must be (None|True|False)')
+
+    ImageDraw.Draw(self.image).ellipse(points, outline=1, fill=fill)
+
+  def draw_line(self, points):
+    """
+    직선을 그립니다.
+
+    example::
+
+      pibo_oled.draw_line((30, 20, 60, 50))
+
+    :param tuple points: 선의 시작 좌표, 선의 끝 좌표 (x1, y1, x2, y2)
+    """
+
+    if type(points) is not tuple:
+      raise Exception(f'"{points}" must be tuple type')
+
+    if len(points) != 4:
+      raise Exception(f'len({points}) must be 4')
+
+    ImageDraw.Draw(self.image).line(points, fill=True)
+
+  def invert(self):
+    """
+    그려진 이미지를 흑백 반전시킵니다.
+
+    example::
+
+      pibo_oled.invert()
+    """
+
+    self.image = ImageOps.invert(self.image.convert("L"))
 
