f0828bd9a0ce1302380f516363e3f7e20594046a
leeyunjai82 <leeyunjai@circul.us>
2024-03-25 17:06:43 +0900

    update

--- docs/build/html/_static/documentation_options.js
+++ docs/build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.9.2.70',
+    VERSION: '0.9.2.71',
     LANGUAGE: 'ko',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
