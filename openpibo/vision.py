a9e82da83397b2db83bb7a400f7a6a9b441299a2
Yunjai, Lee <leeyunjai@circul.us>
2024-07-30 11:26:15 +0900

    Update vision.py

--- openpibo/vision.py
+++ openpibo/vision.py
@@ -1074,21 +1074,18 @@ Functions:
     이미지 안의 객체를 인식합니다. (아래 class의 사물 인식 가능)
 
     인식 가능한 사물은 다음과 같습니다::
-
-      'background', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
-      'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 
-      'None', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 
-      'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'None',
-      'backpack', 'umbrella', 'None', 'None', 'handbag', 'tie', 'suitcase', 'frisbee',
-      'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove',
-      'skateboard', 'surfboard', 'tennis racket', 'bottle', 'None', 'wine glass', 'cup',
-      'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 
-      'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch', 
-      'potted plant', 'bed', 'None', 'dining table', 'None', 'None', 'toilet', 'None', 'tv',
-      'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven',
-      'toaster', 'sink', 'refrigerator', 'None', 'book', 'clock', 'vase', 'scissors',
-      'teddy bear', 'hair drier' 
-
+    {
+      0: 'background', 1:'person',2:'bicycle',3:'car',4:'motorcycle',5:'airplane',6:'bus',7:'train',8:'truck',9:'boat',10:'traffic light',
+      11:'fire hydrant',12:'street sign',13:'stop sign',14:'parking meter',15:'bench',16:'bird',17:'cat',18:'dog',19:'horse',20:'sheep',
+      21:'cow',22:'elephant',23:'bear',24:'zebra',25:'giraffe',26:'hat',27:'backpack',28:'umbrella',29:'shoe',30:'eye glasses',
+      31:'handbag',32:'tie',33:'suitcase',34:'frisbee',35:'skis',36:'snowboard',37:'sports ball',38:'kite',39:'baseball bat',40:'baseball glove',
+      41:'skateboard',42:'surfboard',43:'tennis racket',44:'bottle',45:'plate',46:'wine glass',47:'cup',48:'fork',49:'knife',50:'spoon',
+      51:'bowl',52:'banana',53:'apple',54:'sandwich',55:'orange',56:'broccoli',57:'carrot',58:'hot dog',59:'pizza',60:'donut',
+      61:'cake',62:'chair',63:'couch',64:'potted plant',65:'bed',66:'mirror',67:'dining table',68:'window',69:'desk',70:'toilet',
+      71:'door',72:'tv',73:'laptop',74:'mouse',75:'remote',76:'keyboard',77:'cell phone',78:'microwave',79:'oven',80:'toaster',
+      81:'sink',82:'refrigerator',83:'blender',84:'book',85:'clock',86:'vase',87:'scissors',88:'teddy bear',89:'hair drier',90:'toothbrush',
+      91:'hair brush'
+    }
     example::
 
       img = camera.read()
@@ -1206,9 +1203,9 @@ Functions:
     res = []
     data = data['data'][0].keypoints
 
-    if data[LEFT_WRIST].coordinate.y < data[LEFT_ELBOW].coordinate.y:
+    if data[LEFT_WRIST].coordinate.y < data[LEFT_SHOULDER].coordinate.y:
       res.append("left_hand_up")
-    if data[RIGHT_WRIST].coordinate.y < data[RIGHT_ELBOW].coordinate.y:
+    if data[RIGHT_WRIST].coordinate.y < data[RIGHT_SHOULDER].coordinate.y:
       res.append("right_hand_up")
     if distance(data[LEFT_WRIST].coordinate, data[RIGHT_WRIST].coordinate) <  75:
       res.append("clap")
